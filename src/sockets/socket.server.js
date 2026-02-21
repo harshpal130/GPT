4302bc9cf2c2d3b824c31e54476792b2768b7768
Harsh Pal <harshpal130444@gmail.com>
2026-02-22 02:08:16 +0530

    feat: integrate AI response generation and user authentication via sockets
    - Added user authentication middleware using JWT and cookies in socket server.
    - Implemented AI message handling to store user messages and generate responses using Google GenAI.
    - Created message model for MongoDB to manage chat messages.
    - Introduced ai.service for handling AI interactions and response generation.
    - Updated package.json to include necessary dependencies for cookie handling and Google GenAI.

--- src/sockets/socket.server.js
+++ src/sockets/socket.server.js
@@ -1,11 +1,81 @@
 const { Server } = require("socket.io");
+const cookie = require("cookie")
+const userModel = require("../models/user.model");
+const jwt = require("jsonwebtoken");
+const aiService = require("../services/ai.service")
+const messageModel = require("../models/message.model")
+
 
 function initSocketServer(httpServer){
     const io = new Server(httpServer,{})
 
+    io.use(async(socket, next)=>{
+
+        const cookies = cookie.parse(socket.handshake.headers?.cookie || "");
+
+        if(!cookies.token){
+            next(new Error("authention error no token provided"))
+        }
+
+        try{
+            const decoded = jwt.verify(cookies.token, process.env.JWT_SECRET)
+
+            const user = await userModel.findById(decoded.id)
+            socket.user = user
+
+            next()
+
+        }catch(err){
+            next(new Error("authention error :invalid token"))
+
+        }
+
+    })
+
     io.on("connection",(socket)=>{
-        console.log("new socket connection", socket.id)
+       socket.on("ai-message", async(messagePayLoad)=>{
+
+        console.log(messagePayLoad)
+
+        await messageModel.create({
+            chat:messagePayLoad.chat,
+            user:socket.user._id,
+            content:messagePayLoad.content,
+            role:"user"
+        })
+
+        const chatHistory = await messageModel.find({
+            chat:messagePayLoad.chat
+        })
+
+        
+        
+
+        // messagePayLoad = {
+        //     chat: chat.Id,
+        //     content:message text content
+        // }
+        
+        const response = await aiService.generateResponse(chatHistory.map(item=>{
+            return{
+                role:item.role,
+                parts:[{text:item.content}]
+            }
+        }))
+
+        await messageModel.create({
+            chat:messagePayLoad.chat,
+            user:socket.user._id,
+            content:response,
+            role:"model"
+        })
+
+        socket.emit("ai-response",{
+            content:response,
+            chat:messagePayLoad.chat
+        })
+       })
     })
 }
 
-module.exports= initSocketServer
+module.exports= initSocketServer;
